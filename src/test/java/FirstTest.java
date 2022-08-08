909cb54e7c194acb5a7fe61b21e562fbdd6d8c44
cherrydan <cherrydan.1978@gmail.com>
2022-08-08 12:44:14 +0300

    Runs first test with all dependencies succsessfully!

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FirstTest {
 
+    @Test
+    public void firstTest() {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://demoqa.com/");
+    }
 
 }
