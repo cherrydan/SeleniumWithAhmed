55abbdd4f7627a5d175f716aeee2423eb9e3ef3b
cherrydan <cherrydan.1978@gmail.com>
2022-08-13 12:24:29 +0300

    Found more than one element by cssSelector, clicked on element, found text, convert text to digit and print to cosole!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -18,13 +16,32 @@ public class Main {
         //устанавливаем неявное ожидание
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        driver.get("https://avito.ru");
+        driver.get("https://avito.ru/rossiya");
 
         WebElement element = driver.findElement(By.xpath("(//a[text()='Личные вещи'])[1]"));
 
         String parameter = element.getAttribute("href");
 
         System.out.println('\n'  + parameter);
+
+        WebElement element1 = driver.findElement(By.cssSelector(".category-with-counters-item-HDr9u"));
+
+        String text = element1.getText();
+
+        System.out.println(text);
+
+        element1.click();
+
+        WebElement element2 = driver.findElement(By.cssSelector(".page-title-count-wQ7pG"));
+
+        String amount = element2.getText();
+
+        System.out.printf("%d обьявление в категории %s\n", Integer.valueOf(amount.replaceAll(" ", "")),
+        text);
+
+
+
+
 
 
     }
