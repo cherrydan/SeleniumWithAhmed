6896b4559ae60fff2744e8acc24816e9bee0a116
cherrydan <cherrydan.1978@gmail.com>
2022-08-15 16:43:09 +0300

    Learn about sendKeys method, runs from new package named google_com

---
+++ src/main/java/google_com/Main.java
@@ -0,0 +1,20 @@
+package google_com;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com/");
+
+        WebElement element = driver.findElement(By.xpath("//input[@class='gLFyf gsfi']"));
+
+        element.sendKeys("Курсы по Java", Keys.ENTER);
+    }
+}
