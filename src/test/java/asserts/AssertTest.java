746a6c411a04b42601ce4997004f297b882078ef
cherrydan <cherrydan.1978@gmail.com>
2022-08-23 17:30:03 +0300

    Make first TestNG test in test-scope only

---
+++ src/test/java/asserts/AssertTest.java
@@ -0,0 +1,35 @@
+package asserts;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class AssertTest {
+
+    @Test
+
+    public static void assertTest() {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+
+        //устанавливаем неявное ожидание
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+
+        driver.get("https://avito.ru/rossiya");
+
+        WebElement element1 = driver.findElement(By.cssSelector(".category-with-counters-item-HDr9u"));
+
+        String text = element1.getText();
+
+        Assert.assertEquals(text, "Личные вещи ");
+
+        driver.quit();
+    }
+}
